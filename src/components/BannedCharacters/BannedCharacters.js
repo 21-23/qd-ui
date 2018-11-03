40dcdb391b7f33147c078e519876b7bcf81adea1
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-11-03 20:10:04 +0100

    fix banned characters

--- src/components/BannedCharacters/BannedCharacters.js
+++ src/components/BannedCharacters/BannedCharacters.js
@@ -2,6 +2,44 @@ import React from 'react';
 import classNames from 'classnames';
 import { DisplayConstants, Colors } from './banned-characters-style-constants';
 
+const BannedCharacter = (char, isHighlighted) => {
+    const bannedCharacterClasses = classNames('banned-character', {
+        'highlighted': isHighlighted,
+    });
+
+    return (
+        <div className={bannedCharacterClasses} key={char}>
+            { char }
+
+            <style jsx>{`
+            .banned-character {
+                box-sizing: border-box;
+                font-family: monospace;
+                font-weight: 600;
+                font-size: ${DisplayConstants.FONT_SIZE}px;
+                color: ${Colors.CHARACTER};
+                margin: 0 5px;
+                background-color: ${Colors.CHARACTER_BACKGROUND};
+                min-width: 25px;
+                min-height: 25px;
+                border-radius: 4px;
+                text-align: center;
+                display: inline-flex;
+                align-items: center;
+                justify-content: center;
+                transition: background 0.1s, transform 150ms;
+            }
+
+            .highlighted {
+                background-color: ${Colors.HIGHLIGHTED_CHARACTER_BACKGROUND};
+                border: 2px solid ${Colors.HIGHLIGHTED_CHARACTER_BORDER};
+                transform: scale(${DisplayConstants.HIGHLIGHTED_CHAR_SCALE_COEFFICIENT});
+            }
+            `}</style>
+        </div>
+    );
+};
+
 const BannedCharacters = ({ bannedCharacters = [], highlightedCharacters = [] }) => {
     let content = null;
 
@@ -42,44 +80,6 @@ const BannedCharacters = ({ bannedCharacters = [], highlightedCharacters = [] })
                     display: inline-block;
                     margin-right: 5px;
                 }
-            `}</style>
-        </div>
-    );
-};
-
-const BannedCharacter = (char, isHighlighted) => {
-    const bannedCharacterClasses = classNames('banned-character', {
-        'highlighted': isHighlighted,
-    });
-
-    return (
-        <div className={bannedCharacterClasses}>
-            { char }
-
-            <style jsx>{`
-            .banned-character {
-                box-sizing: border-box;
-                font-family: monospace;
-                font-weight: 600;
-                font-size: ${DisplayConstants.FONT_SIZE}px;
-                color: ${Colors.CHARACTER};
-                margin: 0 5px;
-                background-color: ${Colors.CHARACTER_BACKGROUND};
-                min-width: 25px;
-                min-height: 25px;
-                border-radius: 4px;
-                text-align: center;
-                display: inline-flex;
-                align-items: center;
-                justify-content: center;
-                transition: background 0.1s, transform 150ms;
-            }
-
-            .highlighted {
-                background-color: ${Colors.HIGHLIGHTED_CHARACTER_BACKGROUND};
-                border: 2px solid ${Colors.HIGHLIGHTED_CHARACTER_BORDER};
-                transform: scale(${DisplayConstants.HIGHLIGHTED_CHAR_SCALE_COEFFICIENT});
-            }
             `}</style>
         </div>
     );
