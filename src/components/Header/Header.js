96fbfd01f28082bacb8d09697bddf5b862f7a2e4
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-11-14 19:54:03 +0300

    header design alignments

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,56 @@
 import { h } from 'preact';
 import Icon from 'react-fontawesome';
 
-const Header = ({ productName, username, backgroundImageUrl }) => (
-    <header style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
-        <span>{ productName }</span>
+import { CONTENT_INSET_PADDING } from '../../constants/layout-constants';
+import { Button } from '../Button/Button';
 
-        { username ?
-            <span>
-                <Icon name="user" />
-                <span className="username">{ username }</span>
-            </span>
-            : null
-        }
+const Header = ({ productName, username, backgroundImageUrl, onLogoutButtonClick }) => (
+    <header style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
+        <div className="content-container">
+            <span className="title">{productName}</span>
+
+            {username ?
+                <span className="username">
+                    <Icon name="user" />
+                    <span>{username}</span>
+
+                    <Button text="Logout" iconRight="sign-out" onClick={onLogoutButtonClick} />
+                </span>
+                : null
+            }
+        </div>
         <style jsx>{`
             header {
-                padding: 10px;
-                flex: 0;
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
+                font-family: 'Rosario', sans-serif;
                 background-size: auto 100%;
                 color: white;
                 box-sizing: border-box;
                 border-bottom: 1px solid #49837e;
-                flex-basis: 50px;
+            }
+
+            .content-container {
+                height: 70px;
+                flex: 0;
+                display: flex;
+                flex-basis: 70px;
+                padding: 0 ${CONTENT_INSET_PADDING}px;
+                align-items: center;
+                justify-content: space-between;
+            }
+
+            .title {
+                font-size: 24px;
+                font-weight: bold;
             }
 
             .username {
+                font-size: 18px;
+            }
+
+            .username > span {
                 margin-left: 10px;
+                margin-right: 15px;
+                text-transform: capitalize;
             }
         `}</style>
     </header>
---
+++ src/constants/layout-constants.js
@@ -0,0 +1,2 @@
+export const CONTENT_INSET_PADDING = 20;
+export const CONTENT_MAX_WIDTH = 1280;
